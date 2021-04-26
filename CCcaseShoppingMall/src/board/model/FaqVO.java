2d44f7d39d18707a566ab3ee31b7bef9553825c7
Yeonjae-Cho <whduswo94@gmaill.com>
2021-04-26 16:33:26 +0900

    Faq와 Notice 게시판 VO 생성

---
+++ CCcaseShoppingMall/src/board/model/FaqVO.java
@@ -0,0 +1,78 @@
+package board.model;
+
+public class FaqVO {
+
+	private int faqno;         				 // faq 글번호
+	private String fk_adminid;      // 작성한 관리자아이디
+	private String ftitle;    			     // 제목
+	private String fregisterdate;  // 등록일
+	private String fupdatedate;    // 최근수정일
+	private int number;	        		 // 조회수
+	private String  fcontent;           // 내용
+	
+	public AdminVO adminVO; 	// 관리자VO
+
+	public int getFaqno() {
+		return faqno;
+	}
+	
+	public void setFaqno(int faqno) {
+		this.faqno = faqno;
+	}
+	
+	public String getFk_adminid() {
+		return fk_adminid;
+	}
+	
+	public void setFk_adminid(String fk_adminid) {
+		this.fk_adminid = fk_adminid;
+	}
+	
+	public String getFtitle() {
+		return ftitle;
+	}
+	
+	public void setFtitle(String ftitle) {
+		this.ftitle = ftitle;
+	}
+	
+	public String getFregisterdate() {
+		return fregisterdate;
+	}
+	
+	public void setFregisterdate(String fregisterdate) {
+		this.fregisterdate = fregisterdate;
+	}
+	
+	public String getFupdatedate() {
+		return fupdatedate;
+	}
+	
+	public void setFupdatedate(String fupdatedate) {
+		this.fupdatedate = fupdatedate;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public String getFcontent() {
+		return fcontent;
+	}
+	
+	public void setFcontent(String fcontent) {
+		this.fcontent = fcontent;
+	}
+
+	public AdminVO getAdminVO() {
+		return adminVO;
+	}
+
+	public void setAdminVO(AdminVO adminVO) {
+		this.adminVO = adminVO;
+	}
+}
---
+++ CCcaseShoppingMall/src/board/model/NoticeVO.java
@@ -0,0 +1,80 @@
+package board.model;
+
+public class NoticeVO {
+
+	public int 	noticeno; 					// 공지사항 번호
+	public String fk_adminid;		// 작성자
+	public String ntitle;					// 제목
+	public String nregisterdate;	// 등록일자
+	public String nupdatedate;	// 최근수정일자
+	public int nviewcount;				// 조회수
+	public String ncontent;			// 내용
+	
+	public AdminVO adminVO; 	// 관리자VO
+
+
+	public int getNoticeno() {
+		return noticeno;
+	}
+	
+	public void setNoticeno(int noticeno) {
+		this.noticeno = noticeno;
+	}
+	
+	public String getFk_adminid() {
+		return fk_adminid;
+	}
+	
+	public void setFk_adminid(String fk_adminid) {
+		this.fk_adminid = fk_adminid;
+	}
+	
+	public String getNtitle() {
+		return ntitle;
+	}
+	
+	public void setNtitle(String ntitle) {
+		this.ntitle = ntitle;
+	}
+	
+	public String getNregisterdate() {
+		return nregisterdate;
+	}
+	
+	public void setNregisterdate(String nregisterdate) {
+		this.nregisterdate = nregisterdate;
+	}
+	
+	public String getNupdatedate() {
+		return nupdatedate;
+	}
+	
+	public void setNupdatedate(String nupdatedate) {
+		this.nupdatedate = nupdatedate;
+	}
+	
+	public int getNviewcount() {
+		return nviewcount;
+	}
+	
+	public void setNviewcount(int nviewcount) {
+		this.nviewcount = nviewcount;
+	}
+	
+	public String getNcontent() {
+		return ncontent;
+	}
+	
+	public void setNcontent(String ncontent) {
+		this.ncontent = ncontent;
+	}
+	
+	public AdminVO getAdminVO() {
+		return adminVO;
+	}
+
+	public void setAdminVO(AdminVO adminVO) {
+		this.adminVO = adminVO;
+	}
+
+}
