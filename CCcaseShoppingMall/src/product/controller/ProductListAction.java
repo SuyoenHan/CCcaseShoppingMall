8e2a5f3adac6cba139c5892453963db86bca9e86
SuyeonHan <syflowerdaba@gmail.com>
2021-04-27 21:10:52 +0900

    한수연 > 회사별,기종별 상세페이지 구현중

--- CCcaseShoppingMall/src/product/controller/ProductListAction.java
+++ CCcaseShoppingMall/src/product/controller/ProductListAction.java
@@ -16,10 +16,11 @@ public class ProductListAction extends AbstractController {
 		
 		// =========================== 한수연 시작 ======================================
 		String method= request.getMethod();
+		
 		String mnum= request.getParameter("mnum");
 		String cnum= request.getParameter("cnum");
 		
-		// mnum에 해당하는 회사명 requestScope에 저장
+		// mnum과 mnum에 해당하는 회사명 requestScope에 저장
 		InterMobileCompanyDAO mcdao= new MobileCompanyDAO();
 		String mname= mcdao.getMname(mnum);
 		
@@ -28,9 +29,10 @@ public class ProductListAction extends AbstractController {
 		 	mname!=null 인 경우 mnum에 해당하는 회사명 존재
 		*/
 		request.setAttribute("mname", mname);
+		request.setAttribute("mnum", mnum);
 		
 		
-		// cnum에 해당하는 회사명 requestScope에 저장
+		// cnum과 cnum에 해당하는 카테고리명 requestScope에 저장
 		InterCategoryDAO cdao= new CategoryDAO();
 		String cname= cdao.getCname(cnum);
 		/*
@@ -38,18 +40,40 @@ public class ProductListAction extends AbstractController {
 		 	cname!=null 인 경우 cnum에 해당하는 카테고리명 존재
 	    */
 		request.setAttribute("cname", cname);
+		request.setAttribute("cnum", cnum);
+
 		
-		
-		// mnum과 cnum에 해당하는 필요한 제품 정보들을 map과 list에 담아서 requestScope에 저장
-		//                  (제품번호, 제품명, 기종명, 대표이미지파일명, 제품정가, 할인판매가, 스펙번호 )
 		InterProductDAO pdao= new ProductDAO();
-		List<Map<String,String>> pInfoList= pdao.getProductInfo(mnum, cnum);
+		if(request.getParameter("modelName")!=null && !"all".equals(request.getParameter("modelName"))) { // cnum, mnum, modelName(기종명) 모두 선택한 경우
+		
+			// mnum, cnum, modelName에 해당하는 필요한 제품 정보들을 map과 list에 담아서 requestScope에 저장
+		    //                  (제품번호, 제품명, 기종명, 대표이미지파일명, 제품정가, 할인판매가, 스펙번호, 할인율 )
+			String modelName= request.getParameter("modelName");
+			List<Map<String,String>> pInfoList= pdao.getProductInfo(mnum, cnum,modelName);
+	     /* 
+		 	해당회사, 카테고리, 기종명에 일치하는 제품이 하나도 없는 경우: pInfoList.size() == 0
+		 	해당회사, 카테고리, 기종명에 일치하는 제품이 하나이상 있는 경우: pInfoList.size() > 0  
+		 */
+			request.setAttribute("pInfoList", pInfoList);
+			
+			
+			// select 태그 값 고정을 위해 modelName 넘겨주기
+			request.setAttribute("modelName", modelName);
+			
+		}
+		
+		else { // cnum, mnum만 선택한 경우 또는 [기종을 선택하세요]를 선택한 경우
+		       //                        [기종을 선택하세요]을 선택한 경우 ==> 해당 회사와 카테고리의 모든 기종 다 표시
+			
+			// mnum과 cnum에 해당하는 필요한 제품 정보들을 map과 list에 담아서 requestScope에 저장
+			//                  (제품번호, 제품명, 기종명, 대표이미지파일명, 제품정가, 할인판매가, 스펙번호, 할인율)
+			List<Map<String,String>> pInfoList= pdao.getProductInfo(mnum, cnum);
 		/* 
 	 		해당회사와 카테고리에 일치하는 제품이 하나도 없는 경우: pInfoList.size() == 0
 	 		해당회사와 카테고리에 일치하는 제품이 하나이상 있는 경우: pInfoList.size() > 0  
 		*/
-		
-		request.setAttribute("pInfoList", pInfoList);
+			request.setAttribute("pInfoList", pInfoList);
+		}
 		
 		
 		// mnum과 cnum에 해당하는 기종명을 (중복되는 기종명은 1번만 사용) list에 담아서 requestScope에 저장
--- CCcaseShoppingMall/src/product/model/ProductDAO.java
+++ CCcaseShoppingMall/src/product/model/ProductDAO.java
@@ -76,7 +76,7 @@ public class ProductDAO implements InterProductDAO {
 			conn=ds.getConnection();
 			
 			String sql=" select productid, productname, modelname, pimage1, price " + 
-					   "      , price*(1-salepercent) as saleprice, fk_snum " + 
+					   "      , price*(1-salepercent) as saleprice, fk_snum, salepercent*100 as salepercent " + 
 					   " from " + 
 					   " (select productid, productname, modelname, fk_mnum, fk_cnum from tbl_product) P " + 
 					   " join " + 
@@ -107,10 +107,11 @@ public class ProductDAO implements InterProductDAO {
 				pInfoMap.put("price", price);
 				pInfoMap.put("saleprice", saleprice);
 				
-				if(rs.getInt(7) != -1) { // fk_snum이 존재하는 경우 (== fk_snum이 null값인 경우 sql에서 -1로 변환) 
-					String fk_snum= String.valueOf(rs.getInt(7)); // fk_snum이 0이면 BEST 상품, 1이면 NEW 상품
-					pInfoMap.put("fk_snum", fk_snum);
-				}
+				String fk_snum= String.valueOf(rs.getInt(7)); // fk_snum이 0이면 BEST 상품, 1이면 NEW 상품, -1이면 해당없음
+				pInfoMap.put("fk_snum", fk_snum);
+				
+				String salepercent= String.valueOf(rs.getInt(8));
+				pInfoMap.put("salepercent", salepercent);
 				
 				pInfoList.add(pInfoMap);
 			}
@@ -161,6 +162,75 @@ public class ProductDAO implements InterProductDAO {
 	
 	
 	} // end of public List<String> getModelName(String mnum, String cnum) throws SQLException {----
+
+
+	
+	// mnum, cnum, modelName이 주어진 경우, 
+    // 이에 해당하는 제품번호, 제품명, 기종명, 대표이미지파일명, 제품정가, 할인판매가, 스펙번호 가져오기 (productDetailVO 정보는 하얀색 기준으로..)
+	@Override
+	public List<Map<String, String>> getProductInfo(String mnum, String cnum, String modelName) throws SQLException {
+
+		List<Map<String,String>> pInfoList= new ArrayList<>();
+		
+		try {
+			conn=ds.getConnection();
+			
+			String sql=" select productid, productname, modelname, pimage1, price " +
+				       " , price*(1-salepercent) as saleprice, fk_snum, salepercent*100 as salepercent " + 
+				       " from " +
+				       " (select productid, productname, modelname, fk_mnum, fk_cnum from tbl_product) P " +
+				       " join " +
+				       " (select pimage1, fk_productid, price, salepercent, nvl(fk_snum,-1) as fk_snum from tbl_pdetail where pcolor='white') D " +
+				       " on P.productid = D.fk_productid " +
+				       " where fk_mnum= ? and fk_cnum= ? and modelname= ? ";
+			
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1, Integer.parseInt(mnum));
+			pstmt.setInt(2, Integer.parseInt(cnum));
+			pstmt.setString(3, modelName);
+			rs=pstmt.executeQuery();
+			
+			while(rs.next()) {
+				
+				Map<String,String> pInfoMap= new HashMap<>();
+				
+				String productid= rs.getString(1);
+				String productname= rs.getString(2);
+				String modelname= rs.getString(3);
+				String pimage1= rs.getString(4);
+				String price= String.valueOf(rs.getInt(5));
+				String saleprice= String.valueOf(rs.getInt(6));
+				
+				pInfoMap.put("productid", productid);
+				pInfoMap.put("productname", productname);
+				pInfoMap.put("modelname", modelname);
+				pInfoMap.put("pimage1", pimage1);
+				pInfoMap.put("price", price);
+				pInfoMap.put("saleprice", saleprice);
+				
+				String fk_snum= String.valueOf(rs.getInt(7)); // fk_snum이 0이면 BEST 상품, 1이면 NEW 상품, -1이면 해당없음
+				pInfoMap.put("fk_snum", fk_snum);
+				
+				String salepercent= String.valueOf(rs.getInt(8));
+				pInfoMap.put("salepercent", salepercent);
+				
+				pInfoList.add(pInfoMap);
+	
+			}
+			
+		} finally {
+			close();
+		}
+		
+		
+		return pInfoList;
+		
+		/* 
+		 	해당회사, 카테고리, 기종명에 일치하는 제품이 하나도 없는 경우: pInfoList.size() == 0
+		 	해당회사, 카테고리, 기종명에 일치하는 제품이 하나이상 있는 경우: pInfoList.size() > 0  
+		*/
+		
+	} // end of public List<Map<String, String>> getProductInfo(String mnum, String cnum, String modelName) throws SQLException {----
 
 		
 	// =========================== 한수연 끝 ======================================
