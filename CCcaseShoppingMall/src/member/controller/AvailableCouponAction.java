48529388adfd8027ec63bcb8db3122590468accc
hannahk-coding <jiu94@hanmail.net>
2021-04-29 01:03:19 +0900

    김지우_파일이름 수정

--- CCcaseShoppingMall/src/member/controller/AvailableCouponAction.java
+++ CCcaseShoppingMall/src/member/controller/AvailableCouponAction.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import common.controller.AbstractController;
 
-public class availableCouponAction extends AbstractController {
+public class AvailableCouponAction extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
