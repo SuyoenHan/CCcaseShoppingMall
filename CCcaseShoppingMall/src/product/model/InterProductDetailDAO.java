efaa543ee3993f76e51b71abcd0fbf58fd3130bd
WONBIN <bwb930305@naver.com>
2021-04-29 14:52:50 +0900

    백원빈_admin상품등록 작업

--- CCcaseShoppingMall/src/product/model/InterProductDetailDAO.java
+++ CCcaseShoppingMall/src/product/model/InterProductDetailDAO.java
@@ -1,5 +1,13 @@
 package product.model;
 
-public interface InterProductDetailDAO {
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
+public interface InterProductDetailDAO {
+	
+	//////////////////백원빈 작업시작//////////////////
+	// 전체제품 조회해오기
+	List<Map<String, String>> selectProInfo() throws SQLException;
+	//////////////////백원빈 작업끝//////////////////
 }
