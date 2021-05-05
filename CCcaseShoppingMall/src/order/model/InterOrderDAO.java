302be3f0d440c52179a32c9bea2c021747e20651
SeungHye-J <ivory0130@icloud.com>
2021-05-05 21:02:55 +0900

    승혜- 내 주문내역 미완

--- CCcaseShoppingMall/src/order/model/InterOrderDAO.java
+++ CCcaseShoppingMall/src/order/model/InterOrderDAO.java
@@ -2,17 +2,22 @@ package order.model;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import member.model.MemberVO;
 
 public interface InterOrderDAO {
 
-	
-	// 나의 주문내역  select 해오기
-	List<OrderVO> myOrderList(String userid) throws SQLException;
+	// 나의 주문내역  더보기 페이징 방식으로 select 해오기 -3개월 내의
+	List<OrderVO> selectByOrderList(Map<String, String> paraMap) throws SQLException;
 
+	// Ajax(JSON)를 사용하여 주문내역을 "더보기" 방식으로 페이징처리 해주기 위해  아이디별로 전체개수 알아오기-3개월내의 // 
+	int totalOrderListCount(String userid) throws SQLException;
+	
 	//배송 조회하기
 	List<OrderVO> shipStatusView(String orderno) throws SQLException;
+
+	
 
 	
 }
